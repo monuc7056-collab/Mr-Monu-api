7a5052a97c8b5378ad76fb77e1c8393fc8fb5455
monuc7056-collab <monuc7056@gmail.com>
2026-01-29 11:20:01 +0530

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,1 +1,30 @@
-const filePath = path.join(process.cwd(), "api", "data.json");
+import fs from "fs";
+import path from "path";
+
+export default function handler(req, res) {
+  const num = req.query.num;
+
+  if (!num) {
+    return res.status(400).json({
+      success: false,
+      message: "Please provide a number"
+    });
+  }
+
+  const filePath = path.join(process.cwd(), "data.json");
+  const rawData = fs.readFileSync(filePath);
+  const data = JSON.parse(rawData);
+
+  if (data[num]) {
+    return res.json({
+      success: true,
+      number: num,
+      data: data[num]
+    });
+  } else {
+    return res.json({
+      success: false,
+      message: "Number not found"
+    });
+  }
+}
